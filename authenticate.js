fcc27148b31d4b713e1970764b5852f57a121f55
Kyle Mitchell <kyle@kemitchell.com>
2015-10-05 22:24:12 -0700

    Read passwords JSON file

--- authenticate.js
+++ authenticate.js
@@ -1,3 +1,17 @@
+var fs = require('fs')
+
+var FILE = ( process.env.PASSWORDS_FILE || 'passwords.json' )
+
 module.exports = function authenticate(email, password, callback) {
-  process.nextTick(function() {
-    callback(null, true) }) }
+  fs.readFile(FILE, function(error, buffer) {
+    if (error) {
+      callback(error) }
+    else {
+      var json = JSON.parse(buffer)
+      console.log(email, password)
+      console.log(json)
+      var match = (
+        ( email in json ) &&
+        ( json[email] === password ) )
+      console.log(match)
+      callback(null, match) } }) }
