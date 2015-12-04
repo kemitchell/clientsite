dfe4392d640a552af509c81ff10a7d173f497cf2
Kyle Mitchell <kyle@kemitchell.com>
2015-12-04 00:20:54 -0800

    Base64 encode fonts

--- test/static.test.js
+++ test/static.test.js
@@ -13,27 +13,3 @@ tape('GET /styles.css', function(test) {
         response.headers['content-type'], 'text/css',
         'GET /styles.css serves CSS')
       close() }) }) })
-
-tape('GET /Concourse-C6-Regular.woff', function(test) {
-  test.plan(2)
-  server(function(port, close) {
-    http.get({ path: '/Concourse-C6-Regular.woff', port: port }, function(response) {
-      test.equal(
-        response.statusCode, 200,
-        'GET /Concourse-C6-Regular.woff responds 200')
-      test.equal(
-        response.headers['content-type'], 'application/font-woff',
-        'GET /Concourse-C6-Regular.woff serves WOFF')
-      close() }) }) })
-
-tape('GET /Concourse-T6-Regular.woff', function(test) {
-  test.plan(2)
-  server(function(port, close) {
-    http.get({ path: '/Concourse-T6-Regular.woff', port: port }, function(response) {
-      test.equal(
-        response.statusCode, 200,
-        'GET /Concourse-T6-Regular.woff responds 200')
-      test.equal(
-        response.headers['content-type'], 'application/font-woff',
-        'GET /Concourse-T6-Regular.woff serves WOFF')
-      close() }) }) })
